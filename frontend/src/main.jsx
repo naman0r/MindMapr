3426863086d751d8a0e0131272334d624413b750
Naman Rusia <naman.rusia14@gmail.com>
2025-01-14 16:56:53 -0500

    react router dom is set up and now worksgit add . ! gonna work on the front end for today and start again on backend tomorrow

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -16,6 +16,6 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
