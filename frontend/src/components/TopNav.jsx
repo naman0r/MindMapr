0af6e360a506565a4e0e183b9166305d60f8b31d
Naman Rusia <naman.rusia14@gmail.com>
2025-02-17 01:38:27 -0500

    added circular user profile image from google auth in topnav

--- frontend/src/components/TopNav.jsx
+++ frontend/src/components/TopNav.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 import "../styles/TopNav.css";
+import { useState, useEffect } from "react";
+
+import "./TopNav.css";
+
+import { auth } from "../Firebase";
 
 function TopNav() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        setUser(authUser); // ✅ Set user when logged in
+      } else {
+        setUser(null); // ❌ Set to null when logged out
+      }
+    });
+
+    return () => unsubscribe(); // ✅ Cleanup listener on unmount
+  }, []);
   return (
     <header className="topnav">
       <div className="topnav-left">
@@ -17,7 +35,11 @@ function TopNav() {
       <div className="topnav-right">
         <div className="profile-icon">
           <a href="/profile">
-            <img src="../../public/guesticon.svg" alt="" />
+            <img
+              src={user ? user.photoURL : "../../public/guesticon.svg"}
+              alt=""
+              className="profile-image"
+            />
           </a>
         </div>
       </div>
